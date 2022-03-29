149f9dcc75d1cb5056ab2205ecdc8285ecc17b79
plastikfan <plastikfan@outlook.com>
2022-03-29 15:22:18 +0100

    test: add test to check builder and client build scripts are in sync (#24)

--- Elizium.Build/Tests/Public/Build-Module.tests.ps1
+++ Elizium.Build/Tests/Public/Build-Module.tests.ps1
@@ -1,3 +1,4 @@
+using namespace System.IO;
 
 Describe 'Build-Module' {
   BeforeAll {
@@ -6,11 +7,18 @@ Describe 'Build-Module' {
       -ErrorAction 'stop' -DisableNameChecking;
   }
 
-  Context 'given: blah' {
-    It 'should: ' {
-      InModuleScope Elizium.Build {
-        # Build-Module
-      }
+  Context 'given: module and client task script' {
+    It 'should: ensure they are in sync' {
+      [string]$builder = "Elizium.Build.build.ps1";
+      [string]$client = [Path]::Join(
+        "FileList", "Elizium.Build.tasks.ps1"
+      );
+      $builderHash = (Get-FileHash -Path $builder -Algorithm SHA256).Hash;
+      $clientHash = (Get-FileHash -Path $client -Algorithm SHA256).Hash;
+
+      $builderHash | Should -BeExactly $clientHash -Because $(
+        "Builder and client scripts must be in sync, but are not."
+      );
     }
   }
 }
